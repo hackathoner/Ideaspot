a5c344c246fb199e5e1d42aecddab3034945d224
Anuraag Yachamaneni <hello@anuraag.me>
2015-03-14 20:49:34 -0400

    add files

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = angular.module('ideaspot', []);
+
+app.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.test = 'Welcome to Ideaspot';
+  $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ];
+    $scope.addIdea = function(){
+         if(!$scope.title || $scope.title === '') { return; }
+          $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0
+          });
+          $scope.title = '';
+          $scope.link = '';
+        
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+}]);
+
